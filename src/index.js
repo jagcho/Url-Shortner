dd0102ba683d8eb5e99aa5f5540f3896fcabb995
jagdish M <jagadishchodry@gmail.com>
2023-03-07 12:46:07 +0530

    complrted

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = express.json()
+const mongoose = require("mongoose")
+const app = express()
+const route = require("./routes/route")
+
+app.use(bodyParser)
+
+var cors = require('cors')
+
+app.use(cors())
+
+
+
+mongoose.connect("mongodb+srv://Jagcho:71nEXJtXcYfVx8T6@cluster0.5bg4mzz.mongodb.net/Url-Shortner"
+    , { useNewUrlParser: true })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use("/", route)
+
+
+
+app.listen(process.env.PORT || 5000, function () { console.log("Express is running on port " + (process.env.PORT || 5000)) });
